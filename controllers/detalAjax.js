f4ec78311f1eec9967987c792119a802c3ec1670
nguyenbuitung <nguyenbuitung4@gmail.com>
2022-08-24 21:30:13 +0700

    xong index

--- controllers/detalAjax.js
+++ controllers/detalAjax.js
@@ -1,5 +1,16 @@
 window.onload =function(){
     const urlParams= new URLSearchParams(window.location.search)
-    const myParam=urlParams.get('product')
+    const myParam=urlParams.get('productid')
     console.log('first',myParam)
+    getData(myParam)
 }
+
+async function getData(tung) {
+    console.log("tung", tung)
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
--- controllers/indexAjax.js
+++ controllers/indexAjax.js
@@ -7,7 +7,33 @@ async function getData() {
             method: 'GET',
             ResponseType: 'JSON',
         })
-        renderSinhVien(result.data.content)
+        // render(result.data.content)
+        let dataRender = result.data.content
+        let output = "";
+        for (let item of dataRender) {
+            console.log(item)
+   
+
+            let renderItem = `
+            <div class="col-4">
+                        <div class="item">
+                            <img src="./img/image 5.png" alt="" />
+                            <div class="card-body">
+                                <h1>${item.name}</h1>
+                                <p>${item.description}</p>
+                            </div>
+                            <div class="card-button">
+                                <button><a href="./detail.html?productid=${item.id}">Buy now</a></button>
+                                <span>85$</span>
+                            </div>
+                        </div>
+                    </div>
+            `;
+            output += renderItem
+        }
+        document.querySelector(".product").innerHTML = output
+
+        console.log(dataRender)
     } catch (err) {
         console.log(err)
     }
@@ -17,3 +43,5 @@ window.onload = function () {
     //Gọi api lấy data từ backend
     getData()
 }
+
+
